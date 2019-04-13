ee2582f72fd0b1338dd4542b94f13ad6caff9512
Pavan <pavanjoshi.1234@gmail.com>
2019-04-13 14:32:44 +0530

    First Commit

---
+++ app/src/main/java/pavanjdot/com/uplodedcontentapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package pavanjdot.com.uplodedcontentapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
